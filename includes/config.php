9b1444a41ac67e6f4dc18c9bcd0a65f861d65ceb
ferzt <ferztfrequency@gmail.com>
2022-01-12 16:43:29 -0500

    config changes

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,21 @@
 <?php
+    include("dotenvParser.php");
     ob_start();
     session_start();
 
     $timeZone = date_default_timezone_set("America/New_York");
-//    $con = mysqli_connect("localhost","root","","radiotest");
-    $con = mysqli_connect("us-cdbr-east-04.cleardb.com","b3b579187bfa74","2ea318fb","heroku_06e2a67321279c2");
+
+    (new DotEnv(__DIR__ . '/.env'))->load();
+
+    $host = getenv('MYSQL_HOST');
+
+    $user = getenv('MYSQL_USER');
+        
+    $pass = getenv('MYSQL_PASS');
+
+    $datab = getenv('MYSQL_DAT');
+
+    $con = mysqli_connect($host,$user,$pass,$datab);
 
     
     if(mysqli_connect_errno()) {
